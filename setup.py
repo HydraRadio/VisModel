e5a63508b13d238fb6fa784e1b9b82dc303ad400
Phil Bull <philbull@gmail.com>
2021-01-01 17:11:03 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import json
+import os
+import sys
+from setuptools import setup
+
+#sys.path.append("hera_pspec")
+import version
+
+setup_args = {
+    'name': 'VisModel',
+    'author': 'Phil Bull',
+    'url': 'https://github.com/philbull/VisModel',
+    'license': 'MIT',
+    'version': '0.0.1',
+    'description': 'Visibility forward modelling and statistical sampling.',
+    'packages': ['VisModel'],
+    'package_dir': {'VisModel': 'VisModel'},
+    'install_requires': [
+        'numpy>=1.15',
+        'scipy',
+        'matplotlib>=2.2'
+        'pyuvdata',
+    ],
+    'zip_safe': False,
+}
+
+if __name__ == '__main__':
+    setup(**setup_args)
